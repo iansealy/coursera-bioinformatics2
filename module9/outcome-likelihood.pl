e3b5c44b897277732313a46a0f8c90eb327786b6
Ian Sealy <github@iansealy.com>
2015-06-20 18:44:11 +0100

    Outcome Likelihood.

---
+++ module9/outcome-likelihood.pl
@@ -0,0 +1,248 @@
+#!/usr/bin/env perl
+
+# PODNAME: outcome-likelihood.pl
+# ABSTRACT: Outcome Likelihood
+
+## Author     : Ian Sealy
+## Maintainer : Ian Sealy
+## Created    : 2015-06-20
+
+use warnings;
+use strict;
+use autodie;
+use Getopt::Long;
+use Pod::Usage;
+use Carp;
+use Path::Tiny;
+use version; our $VERSION = qv('v0.1.0');
+
+# Default options
+my $input_file = 'outcome-likelihood-sample-input.txt';
+my ( $debug, $help, $man );
+
+# Get and check command line options
+get_and_check_options();
+
+my ( $emissions, undef, $alphabet, undef, $states, undef, @matrix ) =
+  path($input_file)->lines( { chomp => 1 } );
+my @emissions = split //xms,    $emissions;
+my @alphabet  = split /\s+/xms, $alphabet;
+my @states    = split /\s+/xms, $states;
+
+my $transition = get_transitions( \@matrix );
+my $emission   = get_emissions( \@matrix );
+
+printf "%.11e\n", viterbi( $transition, $emission, \@states, \@emissions );
+
+# Get transitions from input data
+sub get_transitions {
+    my ($matrix) = @_;
+
+    my $transition = {};    ## no critic (ProhibitReusedNames)
+
+    ## no critic (ProhibitReusedNames)
+    my @states = grep { length $_ } split /\s+/xms, shift @{$matrix};
+    ## use critic
+    while ( my $line = shift @{$matrix} ) {
+        last if $line eq '--------';
+        my @fields = split /\s+/xms, $line;
+        my $state1 = shift @fields;
+        foreach my $state2 (@states) {
+            $transition->{$state1}{$state2} = shift @fields;
+        }
+    }
+
+    # Initial transitions
+    foreach my $state (@states) {
+        $transition->{q{}}{$state} = 1 / scalar @states;
+    }
+
+    return $transition;
+}
+
+# Get emissions from input data
+sub get_emissions {
+    my ($matrix) = @_;
+
+    my $emission = {};    ## no critic (ProhibitReusedNames)
+
+    ## no critic (ProhibitReusedNames)
+    my @alphabet = grep { length $_ } split /\s+/xms, shift @{$matrix};
+    ## use critic
+    while ( my $line = shift @{$matrix} ) {
+        my @fields = split /\s+/xms, $line;
+        my $state = shift @fields;
+        foreach my $letter (@alphabet) {
+            $emission->{$state}{$letter} = shift @fields;
+        }
+    }
+
+    return $emission;
+}
+
+# Viterbi algorithm
+sub viterbi {
+    ## no critic (ProhibitReusedNames)
+    my ( $transition, $emission, $states, $emissions ) = @_;
+    ## use critic
+
+    my $s         = {};
+    my $forward   = {};
+    my $backtrack = {};
+    for my $state ( @{$states} ) {
+        $s->{$state}[0] =
+          $transition->{q{}}{$state} * $emission->{$state}{ $emissions->[0] };
+        $forward->{$state}[0]   = $s->{$state}[0];
+        $backtrack->{$state}[0] = undef;
+    }
+
+    foreach my $i ( 1 .. ( scalar @{$emissions} ) - 1 ) {
+        for my $state1 ( @{$states} ) {
+            my $max = 0;
+            my $sum = 0;
+            for my $state2 ( @{$states} ) {
+                my $product_weight =
+                  $s->{$state2}[ $i - 1 ] *
+                  $transition->{$state2}{$state1} *
+                  $emission->{$state1}{ $emissions->[$i] };
+                if ( $product_weight > $max ) {
+                    $max = $product_weight;
+                    $backtrack->{$state1}[$i] = $state2;
+                }
+                $sum +=
+                  $forward->{$state2}[ $i - 1 ] *
+                  $transition->{$state2}{$state1} *
+                  $emission->{$state1}{ $emissions->[$i] };
+            }
+            $s->{$state1}[$i]       = $max;
+            $forward->{$state1}[$i] = $sum;
+        }
+    }
+
+    my $prob = 0;
+    foreach my $state ( @{$states} ) {
+        $prob += $forward->{$state}[-1];
+    }
+
+    return $prob;
+}
+
+# Get and check command line options
+sub get_and_check_options {
+
+    # Get options
+    GetOptions(
+        'input_file=s' => \$input_file,
+        'debug'        => \$debug,
+        'help'         => \$help,
+        'man'          => \$man,
+    ) or pod2usage(2);
+
+    # Documentation
+    if ($help) {
+        pod2usage(1);
+    }
+    elsif ($man) {
+        pod2usage( -verbose => 2 );
+    }
+
+    return;
+}
+
+__END__
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+outcome-likelihood.pl
+
+Outcome Likelihood
+
+=head1 VERSION
+
+version 0.1.0
+
+=head1 DESCRIPTION
+
+This script solves the Outcome Likelihood Problem.
+
+Input: A string I<x>, followed by the alphabet from which I<x> was constructed,
+followed by the states I<States>, transition matrix I<Transition>, and emission
+matrix I<Emission> of an HMM (I<Σ>, I<States>, I<Transition>, I<Emission>).
+
+Output: The probability Pr(I<x>) that the HMM emits I<x>.
+
+=head1 EXAMPLES
+
+    perl outcome-likelihood.pl
+
+    perl outcome-likelihood.pl --input_file outcome-likelihood-extra-input.txt
+
+    diff <(perl outcome-likelihood.pl) outcome-likelihood-sample-output.txt
+
+    diff \
+        <(perl outcome-likelihood.pl \
+            --input_file outcome-likelihood-extra-input.txt) \
+        outcome-likelihood-extra-output.txt
+
+    perl outcome-likelihood.pl --input_file dataset_11594_8.txt \
+        > dataset_11594_8_output.txt
+
+=head1 USAGE
+
+    outcome-likelihood.pl
+        [--input_file FILE]
+        [--debug]
+        [--help]
+        [--man]
+
+=head1 OPTIONS
+
+=over 8
+
+=item B<--input_file FILE>
+
+The input file containing "A string I<x>, followed by the alphabet from which
+I<x> was constructed, followed by the states I<States>, transition matrix
+I<Transition>, and emission matrix I<Emission> of an HMM (I<Σ>, I<States>,
+I<Transition>, I<Emission>)".
+
+=item B<--debug>
+
+Print debugging information.
+
+=item B<--help>
+
+Print a brief help message and exit.
+
+=item B<--man>
+
+Print this script's manual page and exit.
+
+=back
+
+=head1 DEPENDENCIES
+
+None
+
+=head1 AUTHOR
+
+=over 4
+
+=item *
+
+Ian Sealy
+
+=back
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is Copyright (c) 2015 by Ian Sealy.
+
+This is free software, licensed under:
+
+  The GNU General Public License, Version 3, June 2007
+
+=cut
